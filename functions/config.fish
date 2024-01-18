c35feb85706549e747e511c16a46e743ad325163
Leenuus <linusrach24@gmail.com>
2024-01-18 23:37:54 +0800

    Better config editor

--- functions/config.fish
+++ functions/config.fish
@@ -3,24 +3,41 @@ function config
     set options $options (fish_opt -s m -l mpv)
     set options $options (fish_opt -s f -l fish)
     set options $options (fish_opt -s n -l nvim)
+    set options $options (fish_opt -s a -l alacritty)
+    set options $options (fish_opt -s l -l lvim)
+    set options $options (fish_opt -s s -l ssh)
+    set options $options (fish_opt -s x -l tmuxifier)
+    set options $options (fish_opt -s d -l directory)
     argparse $options -- $argv
 
-    if test (which lvim)
-        set editor lvim
-    else
-        set editor $EDITOR
-    end
-
     if test -n "$_flag_tmux"
-      echo "_flag_tmux:.$_flag_tmux..."
-        $editor $HOME/.config/tmux/tmux.conf
+        set config_dir $HOME/.config/tmux/tmux.conf
     else if test -n "$_flag_mpv"
-        $editor $HOME/.config/mpv
+        set config_dir $HOME/.config/mpv
     else if test -n "$_flag_fish"
-        $editor $HOME/.config/fish
+        set config_dir $HOME/.config/fish
     else if test -n "$_flag_nvim"
-        $editor $HOME/.config/nvim
+        set config_dir $HOME/.config/nvim
+    else if test -n "$_flag_a"
+        set config_dir $HOME/.config/alacritty/
+    else if test -n "$_flag_lvim"
+        set config_dir $HOME/.local/share/lunarvim/lvim/
+    else if test -n "$_flag_ssh"
+        set config_dir $HOME/.ssh/config
+    else if test -n "$_flag_tmuxifier"
+        set config_dir $HOME/.config/tmux/plugins/tmuxifier/layouts/
     else
         cd $HOME/.config/
+        return
+    end
+
+    if test -n "_flag_d"
+        cd "$config_dir"
+    end
+
+    if test -n "$editor"
+        $editor "$config_dir"
+    else
+        $EDITOR "$config_dir"
     end
 end
